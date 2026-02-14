a8503b35924b2226a59c8282de43445d18200e47
Jelle Metzlar <github@metzlar.net>
2026-02-14 15:27:39 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,360 @@
+#include <Arduino.h>
+#include "esp_system.h"
+#include <TMCStepper.h>
+#include <AccelStepper.h>
+#include "OneButton.h"
+#include "WiFi.h"
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>
+#include "secrets.h"
+
+
+#define MAX_SPEED (180 * steps_per_mm)
+#define STALL_VALUE 50 // [0..255]
+#define EN_DIR_STEP_OUTPUT 1
+
+#define R_SENSE 0.11f
+
+#define EN_PIN 2    // enable (CFG6)
+#define DIR_PIN 18  // direction
+#define STEP_PIN 23 // step
+
+#define CLK_PIN 19
+#define SPREAD_PIN 4
+
+#define SW_RX 26 // TMC2208/TMC2224 SoftwareSerial receive pin
+#define SW_TX 27 // TMC2208/TMC2224 SoftwareSerial transmit pin
+
+#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
+
+#define BTN1 36 // NEXT
+#define BTN2 34 // ENTER
+#define BTN3 35 // MENU
+
+#define SPI_MT_CS 15 // MT6816
+#define SPI_CLK 14
+#define SPI_MISO 12
+#define SPI_MOSI 13
+
+#define IIC_SCL 21
+#define IIC_SDA 22
+
+#define iStep 25
+#define iDIR 32
+#define iEN 33
+
+HardwareSerial SerialDriver(Serial1);
+TMC2209Stepper driver(&SerialDriver, R_SENSE, DRIVER_ADDRESS);
+AccelStepper stepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
+OneButton button1(BTN1, true);
+OneButton button2(BTN2, true);
+OneButton button3(BTN3, true);
+SPIClass MT6816;
+
+constexpr uint32_t steps_per_mm = 80;
+
+static double Monitor_Speed;
+
+int8_t Direction = 1;
+double _lastLocation = 0;
+double _currentLocation = 0;
+uint32_t last_ota_time = 0;
+uint8_t stallCount = 0;
+
+void motor_init(void);
+void MT6816_init(void);
+int MT6816_read(void);
+double ReadSpeed(float ms);
+void Position1();
+void Position2();
+void Stop();
+void Task1(void *pvParameters);
+void Task2(void *pvParameters);
+
+IPAddress server(192, 168, 1, 9);
+WiFiClient espClient;
+
+void MqttCallback(char *topic, byte *payload, unsigned int length)
+{
+}
+
+PubSubClient pubSubClient(server, 1883, MqttCallback, espClient);
+
+TimerHandle_t sensorTimer;
+void PublishSensors(TimerHandle_t xTimer) 
+{
+    if (!pubSubClient.connected()) {
+        return;
+    }
+
+    JsonDocument doc;
+    
+    doc["position"] = stepper.currentPosition();
+    doc["running"] = stepper.isRunning();
+    doc["stallguard"] = driver.SG_RESULT();
+    doc["rms"] = driver.cs2rms(driver.cs_actual());
+    doc["version"] = driver.version();
+    doc["IOIN"] = driver.IOIN();
+
+    char buffer[256];
+    serializeJson(doc, buffer);
+
+    pubSubClient.publish("test", buffer);
+}
+
+void setup()
+{
+    motor_init();
+    xTaskCreatePinnedToCore(Task1, "Task1", 1024 * 10, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(Task2, "Task2", 1024 * 20, NULL, 2, NULL, 0);
+    sensorTimer = xTimerCreate(
+        "PublishSensors",                   // Timer name
+        pdMS_TO_TICKS( 1000 ),      // 1s period
+        pdTRUE,                         // Auto-reload (periodic timer)
+        NULL,                           // Timer ID
+        PublishSensors                   // Callback function
+    );
+
+    xTimerStart(sensorTimer, pdMS_TO_TICKS( 5000 ));
+}
+
+void loop()
+{
+    ArduinoOTA.handle();
+    stepper.run();
+    pubSubClient.loop();
+}
+
+void Task1(void *pvParameters)
+{
+    MT6816_init();
+
+    button1.attachClick(Position1);
+    button2.attachClick(Position2);
+    button3.attachClick(Stop);
+
+    while (1)
+    {
+        Monitor_Speed = ReadSpeed(100);
+        button1.tick();
+        button2.tick();
+        button3.tick();
+        delay(100);
+    }
+}
+
+void Task2(void *pvParameters)
+{
+    bool isConnected = false;
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    Serial.println("scan start");
+
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0)
+    {
+        Serial.println("no networks found");
+    }
+    else
+    {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i)
+        {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+            delay(10);
+        }
+
+        if (!isConnected)
+        {
+            WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        }
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            delay(500);
+            Serial.print(".");
+        }
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        isConnected = true;
+    }
+
+    Serial.println("");
+
+    ArduinoOTA
+        .onStart([]()
+                 {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type); })
+        .onEnd([]()
+               { Serial.println("\nEnd"); })
+        .onProgress([](unsigned int progress, unsigned int total)
+                    {
+      if (millis() - last_ota_time > 500) {
+        Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+        last_ota_time = millis();
+      } })
+        .onError([](ota_error_t error)
+                 {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      } });
+    ArduinoOTA.begin();
+
+    if (pubSubClient.connect(DEVICE_ID, MQTT_USER, MQTT_PASS))
+    {
+        pubSubClient.publish("test", "hello world");
+    }
+
+    vTaskDelete(NULL);
+}
+
+void MT6816_init(void)
+{
+    MT6816.begin(SPI_CLK, SPI_MISO, SPI_MOSI, SPI_MT_CS);
+    pinMode(SPI_MT_CS, OUTPUT);
+    MT6816.setClockDivider(SPI_CLOCK_DIV4);
+    _lastLocation = (double)MT6816_read();
+}
+
+int MT6816_read(void)
+{
+    uint16_t temp[2];
+    digitalWrite(SPI_MT_CS, LOW);
+    MT6816.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE3));
+    temp[0] = MT6816.transfer16(0x8300) & 0xFF;
+    MT6816.endTransaction();
+    digitalWrite(SPI_MT_CS, HIGH);
+
+    digitalWrite(SPI_MT_CS, LOW);
+    MT6816.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE3));
+    temp[1] = MT6816.transfer16(0x8400) & 0xFF;
+    MT6816.endTransaction();
+    digitalWrite(SPI_MT_CS, HIGH);
+    return (int)(temp[0] << 6 | temp[1] >> 2);
+}
+
+double ReadSpeed(float ms)
+{
+    double Speed_t = 0;
+    _currentLocation = (double)MT6816_read();
+
+    if (_currentLocation == _lastLocation)
+        Speed_t = Direction = 0;
+    else
+    {
+        double temp_t = abs(_currentLocation - _lastLocation);
+        if (temp_t < 8192)
+        { // The displacement per unit time above the maximum is considered to be reversed
+            Speed_t = (temp_t * 360) / 16384;
+            Direction = _currentLocation > _lastLocation ? 1 : -1;
+        }
+        else
+        {
+            Speed_t = ((_currentLocation > _lastLocation ? 16384 - _currentLocation + _lastLocation : 16384 - _lastLocation + _currentLocation) * 360) / 16384;
+            Direction = _currentLocation > _lastLocation ? -1 : 1;
+        }
+    }
+    Speed_t = Direction * (Speed_t * ms / 1000);
+    _lastLocation = _currentLocation;
+    return Speed_t;
+}
+
+void motor_init(void)
+{
+    pinMode(CLK_PIN, OUTPUT);
+    pinMode(SPREAD_PIN, OUTPUT);
+    digitalWrite(CLK_PIN, LOW);
+    digitalWrite(SPREAD_PIN, HIGH);
+
+    pinMode(EN_PIN, OUTPUT);
+    pinMode(STEP_PIN, OUTPUT);
+    pinMode(DIR_PIN, OUTPUT);
+    digitalWrite(EN_PIN, LOW); // Enable driver in hardware
+
+    SerialDriver.begin(115200, SERIAL_8N1, SW_RX, SW_TX);
+    driver.begin(); //  SPI: Init CS pins and possible SW SPI pins
+    // UART: Init SW UART (if selected) with default 115200 baudrate
+    driver.push();
+    driver.pdn_disable(true);
+    driver.mstep_reg_select(1);
+    driver.blank_time(24);
+    driver.toff(5);           // Enables driver in software
+    driver.rms_current(1600); // Set motor RMS current
+    driver.microsteps(8);     // Set microsteps to 1/16th
+    driver.ihold(0);
+    driver.iholddelay(10);
+    driver.freewheel(1);
+    driver.TPOWERDOWN(100);
+
+    driver.en_spreadCycle(false); // Toggle spreadCycle on TMC2208/2209/2224
+    driver.pwm_autoscale(true);   // Needed for stealthChop
+
+    // stalling
+    driver.TCOOLTHRS(0xFFFFF); // 20bit max
+    driver.semin(5);
+    driver.semax(2);
+    driver.sedn(0b01);
+    driver.SGTHRS(STALL_VALUE);
+
+    // driver->TSTEP();
+    stepper.setMaxSpeed(MAX_SPEED);              // 100mm/s @ 80 steps/mm
+    stepper.setAcceleration(500); // 2000mm/s^2
+    stepper.setEnablePin(EN_PIN);
+    stepper.setPinsInverted(false, false, true);
+}
+
+void Position1()
+{
+    stepper.enableOutputs();
+    stepper.moveTo(stepper.currentPosition() + 200);
+}
+
+void Position2()
+{
+    stepper.enableOutputs();
+    stepper.moveTo(stepper.currentPosition() - 200);
+}
+
+void Stop()
+{
+    stepper.stop();
+    while (stepper.isRunning())
+    {
+        stepper.run();
+    }
+    stepper.disableOutputs(); // ⭐ freespin
+}
+
+void PerformHoming()
+{
+}
